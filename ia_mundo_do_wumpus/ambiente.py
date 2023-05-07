c6ff487574d907a7fce793e859ef2ab903c0a39f
Oseiasdfarias <oseias.dfarias@gmail.com>
2023-05-06 23:26:16 -0300

    repositorio atualizado

--- ia_mundo_do_wumpus/ambiente.py
+++ ia_mundo_do_wumpus/ambiente.py
@@ -1,4 +1,5 @@
 import numpy as np
+from random import randint
 
 
 class Ambiente:
@@ -12,18 +13,31 @@ class Ambiente:
 
         self.mundo = np.chararray(dimensoes)
         self.mundo[:] = "0"
+        # Posicionando o Wumpus no Mapa
+        self.pos_wumpus()
 
         # Posicionar Agente
         self.pos_agente()
 
     def pos_wumpus(self):
-        pass
+        """Posiciona o Wumpus no Mapa!"""
+        pos_sort = self.sortear_pos()
+        if self.mundo[pos_sort[0], pos_sort[1]] == b"0":
+            self.mundo[pos_sort[0], pos_sort[1]] = b"W"
+            print(self.mundo[pos_sort[0], pos_sort[1]])
+        else:
+            self.pos_wumpus()
 
     def pos_poco(self):
         pass
 
     def pos_ouro(self):
         pass
+
+    def sortear_pos(self):
+        x = randint(1, self.dimensoes[0]-1)
+        y = randint(1, self.dimensoes[0]-1)
+        return np.array([x, y])
 
     def pos_agente(self):
         self.mundo[0, 0] = "A"
@@ -39,3 +53,4 @@ if __name__ == "__main__":
     amb = Ambiente((3, 3))
     amb.infos_ambiente()
     amb.mostrar_ambiente()
+
