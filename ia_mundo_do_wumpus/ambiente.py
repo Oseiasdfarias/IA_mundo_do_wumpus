ad3d2294e061c73cfec8a61681be4eb78b44c825
Oseiasdfarias <oseias.dfarias@gmail.com>
2023-05-06 23:39:53 -0300

    repositorio atualizado

--- ia_mundo_do_wumpus/ambiente.py
+++ ia_mundo_do_wumpus/ambiente.py
@@ -4,34 +4,34 @@ from random import randint
 
 class Ambiente:
     """Classe que implementa o Anbiente do Mundo do Wumpus."""
-    def __init__(self, dimensoes: tuple = (3, 3), poco: int = 3,
+    def __init__(self, dimensao_ambiente: int = 3, poco: int = 3,
                  wumpus: int = 1, ouro: int = 1) -> None:
         self.poco = poco
         self.wumpus = wumpus
         self.ouro = ouro
-        self.dimensoes = dimensoes
+        self.dimensoes = (dimensao_ambiente, dimensao_ambiente)
 
-        self.mundo = np.chararray(dimensoes)
+        self.mundo = np.chararray(self.dimensoes)
         self.mundo[:] = "0"
         # Posicionando o Wumpus no Mapa
-        self.pos_wumpus()
+        self.add_pos_wumpus()
 
         # Posicionar Agente
-        self.pos_agente()
+        self.add_pos_agente()
 
-    def pos_wumpus(self):
-        """Posiciona o Wumpus no Mapa!"""
+    def add_pos_wumpus(self):
+        """Posiciona o Wumpus no Ambiente."""
         pos_sort = self.sortear_pos()
         if self.mundo[pos_sort[0], pos_sort[1]] == b"0":
             self.mundo[pos_sort[0], pos_sort[1]] = b"W"
             print(self.mundo[pos_sort[0], pos_sort[1]])
         else:
-            self.pos_wumpus()
+            self.add_pos_wumpus()
 
-    def pos_poco(self):
+    def add_pos_poco(self):
         pass
 
-    def pos_ouro(self):
+    def add_pos_ouro(self):
         pass
 
     def sortear_pos(self):
@@ -39,7 +39,7 @@ class Ambiente:
         y = randint(1, self.dimensoes[0]-1)
         return np.array([x, y])
 
-    def pos_agente(self):
+    def add_pos_agente(self):
         self.mundo[0, 0] = "A"
 
     def infos_ambiente(self) -> None:
@@ -50,7 +50,6 @@ class Ambiente:
 
 
 if __name__ == "__main__":
-    amb = Ambiente((3, 3))
+    amb = Ambiente(dimensao_ambiente=3)
     amb.infos_ambiente()
     amb.mostrar_ambiente()
-
