1b65cc24c24583c84efffb5f0b7e93b365c92cb1
Oseiasdfarias <oseias.dfarias@gmail.com>
2023-05-06 21:14:18 -0300

    repositorio atualizado

--- ia_mundo_do_wumpus/ambiente.py
+++ ia_mundo_do_wumpus/ambiente.py
@@ -3,9 +3,30 @@ import numpy as np
 
 class Ambiente:
     """Classe que implementa o Anbiente do Mundo do Wumpus."""
-    def __init__(self, dimesoes: tuple = (3, 3)) -> None:
-        self.mundo = np.chararray(dimesoes)
+    def __init__(self, dimensoes: tuple = (3, 3), poco: int = 3,
+                 wumpus: int = 1, ouro: int = 1) -> None:
+        self.poco = poco
+        self.wumpus = wumpus
+        self.ouro = ouro
+        self.dimensoes = dimensoes
+
+        self.mundo = np.chararray(dimensoes)
         self.mundo[:] = "0"
+
+        # Posicionar Agente
+        self.pos_agente()
+
+    def pos_wumpus(self):
+        pass
+
+    def pos_poco(self):
+        pass
+
+    def pos_ouro(self):
+        pass
+
+    def pos_agente(self):
+        self.mundo[0, 0] = "A"
 
     def infos_ambiente(self) -> None:
         print(f"Tamanho do Anbiente: {self.mundo.shape}")
@@ -13,12 +34,8 @@ class Ambiente:
     def mostrar_ambiente(self) -> None:
         print(f"\nMundo do Wumpus:\n{self.mundo}\n")
 
-    def modificar_ambiente(self, pos: list = [0, 0], add: str = "x") -> None:
-        self.mundo[pos[0], pos[1]] = add
-
 
 if __name__ == "__main__":
     amb = Ambiente((3, 3))
     amb.infos_ambiente()
-    amb.modificar_ambiente(add="W")
     amb.mostrar_ambiente()
