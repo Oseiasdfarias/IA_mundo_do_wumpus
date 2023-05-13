8721a96e5d63deac3df4a21a12b4bb1f3a10b5b1
Oseiasdfarias <oseias.dfarias@gmail.com>
2023-05-13 12:15:57 -0300

    repositorio atualizado

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -14,12 +14,12 @@ const config = {
   url: 'https://oseiasdfarias.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'oseiasdfarias', // Usually your GitHub org/user name.
-  projectName: 'IA_mundo_do_wumpus/docs', // Usually your repo name.
+  projectName: 'IA_mundo_do_wumpus', // Usually your repo name.
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
