ec147658ef7a1dfd15c8a0a63afab2c4f43eaab0
Oseiasdfarias <oseias.dfarias@gmail.com>
2023-05-13 12:18:29 -0300

    repositorio atualizado

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'oseiasdfarias', // Usually your GitHub org/user name.
-  projectName: 'IA_mundo_do_wumpus', // Usually your repo name.
+  projectName: 'docs', // Usually your repo name.
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
