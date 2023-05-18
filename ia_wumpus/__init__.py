afca8b987709fed4b9cfe47de9e1d32fa6470da6
Oseiasdfarias <oseias.dfarias@gmail.com>
2023-05-18 13:12:08 -0300

    repositorio atualizado

--- ia_wumpus/__init__.py
+++ ia_wumpus/__init__.py
@@ -1,3 +1,20 @@
+# -----------------------------------------------------
+# Universidade Federal do Pará
+# Campus Universitário de Tucuruí
+# Faculdade de Engenharia Elétrica
+# -----------------------------------------------------
+#
+# Disciplina: Inteligência Computacional
+# Projeto: O Mundo do Wumpus
+#
+# Professor: Otávio Teixeira
+# Discentes: Ângelo Aragão,
+#            Johanes Martins,
+#            Oséias Farias
+#
+# Data: Maio - 2023
+#  ----------------------------------------------------
+
 from .ambiente import Ambiente  # noqa: F401
 
 __version__ = "0.1.0"
