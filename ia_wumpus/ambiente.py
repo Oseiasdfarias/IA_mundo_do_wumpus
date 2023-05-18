d6d1e0394d2520033800e2ad1fbb731623493f57
Oseiasdfarias <oseias.dfarias@gmail.com>
2023-05-18 11:55:23 -0300

    repositorio atualizado

--- ia_wumpus/ambiente.py
+++ ia_wumpus/ambiente.py
@@ -1,7 +1,7 @@
 import numpy as np
 from random import randint
 import numpy.typing as npt
-from typing import Tuple
+from typing import Tuple, Dict
 
 
 class Ambiente:
@@ -23,6 +23,10 @@ class Ambiente:
         self.percepcoes: dict = {"pocos":  [],
                                  "wumpus": [],
                                  "ouro":   []}
+        self.pos_objetos: dict = {"pocos":  [],
+                                  "wumpus": [],
+                                  "ouro":   [],
+                                  "agente": []}
 
         # Posicionando o Agente no ambiente.
         self.__add_pos_agente()
@@ -38,23 +42,33 @@ class Ambiente:
     def get_mundo(self) -> npt.NDArray:
         return self.__mundo
 
-    def set_agente(self, pos_agente: Tuple[int]) -> None:
-        self.__mundo[pos_agente] = 4
+    def atualiza_pos_agente(self, pos_agente: Tuple[int, int]) -> None:
+        self.__mundo[self.pos_objetos["agente"][-1]] = 0
+        print("antes ", self.pos_objetos["agente"][-1])
+        self.pos_objetos["agente"] = [pos_agente]
+        print("depois ", self.pos_objetos["agente"][-1])
+        self.__mundo[self.pos_objetos["agente"][-1]] = 4
 
-    def get_percepcoes(self):
+    def get_pos_objetos(self) -> Dict:
+        return self.pos_objetos
+
+    def get_percepcoes(self) -> Dict:
         return self.percepcoes
 
-    def add_pos_obj(self, obj: int) -> npt.NDArray:
+    def add_pos_obj_map(self, obj: int) -> npt.NDArray:
         """Posiciona os Objetos no Ambiente."""
         self.pos_sort = self.__sortear_pos()
         if self.__mundo[self.pos_sort[0], self.pos_sort[1]] == 0:
             self.__mundo[self.pos_sort[0], self.pos_sort[1]] = obj
             return self.pos_sort
         elif ((self.pos_sort[0] == 0) and (self.pos_sort[1] == 0)):
-            self.add_pos_obj(obj)
+            self.add_pos_obj_map(obj)
         else:
-            self.add_pos_obj(obj)
+            self.add_pos_obj_map(obj)
         return self.pos_sort
+
+    def salvar_pos_objetos(self, objeto: str, pos_objeto) -> None:
+        self.pos_objetos[objeto].append(pos_objeto)
 
     def __sortear_pos(self) -> npt.NDArray:
         """Sortea as posições dos Objetos no Ambiente."""
@@ -83,27 +97,31 @@ class Ambiente:
     def __add_pos_wumpus(self) -> None:
         """Posicionando os Wumpo(s) no Ambiente."""
         for i in range(self.wumpus):
-            pos_wumpus = self.add_pos_obj(1)
+            pos_wumpus = self.add_pos_obj_map(1)
+            self.salvar_pos_objetos(objeto="wumpus", pos_objeto=pos_wumpus)
             self.__add_percepcoes_obj(objeto="wumpus", pos=pos_wumpus)
             # print(f"Wumpus pos: {pos_wumpus}")
 
     def __add_pos_pocos(self) -> None:
         """Posicionando os Poços no Ambiente."""
         for i in range(self.pocos):
-            pos_poco = self.add_pos_obj(2)
+            pos_poco = self.add_pos_obj_map(2)
+            self.salvar_pos_objetos(objeto="pocos", pos_objeto=pos_poco)
             self.__add_percepcoes_obj(objeto="pocos", pos=pos_poco)
             # print(f"Poço pos: {pos_poco}")
 
     def __add_pos_ouro(self) -> None:
         """Posicionando o(s) Ouro(s) no Ambiente."""
         for i in range(self.ouro):
-            pos_ouro = self.add_pos_obj(3)
+            pos_ouro = self.add_pos_obj_map(3)
+            self.salvar_pos_objetos(objeto="ouro", pos_objeto=pos_ouro)
             self.percepcoes["ouro"].append((pos_ouro[1], pos_ouro[0]))
             # print(f"Ouro pos: {pos_ouro}")
 
     def __add_pos_agente(self) -> None:
         """Posicionando o(s) Agente(s) no Ambiente."""
-        self.__mundo[0, 0] = 4
+        self.__mundo[(0, 0)] = 4
+        self.salvar_pos_objetos(objeto="agente", pos_objeto=((0, 0)))
 
     @classmethod
     def __menu(self) -> None:
@@ -133,8 +151,9 @@ class Ambiente:
 
 
 if __name__ == "__main__":
-    amb = Ambiente(dimensao_ambiente=3)
+    amb = Ambiente(dimensao_ambiente=5)
     # amb.infos_ambiente()
     amb.mostrar_ambiente()
-    amb.mostrar_percepcoes()
-    amb.set_agente((1, 1))
+    # amb.mostrar_percepcoes()
+    amb.atualiza_pos_agente((1, 1))
+    amb.mostrar_ambiente()
